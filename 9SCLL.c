684f1137536a1061a3373ccb1740f0491964b2cc
Tejas <tejas@kali.Tejas>
2025-12-01 22:31:12 +0530

    DS

--- 9SCLL.c
+++ 9SCLL.c
@@ -4,24 +4,23 @@
 
 #define COMPARE(x, y) ((x == y) ? 0 : (x > y ? 1 : -1))
 
-// Node for storing one polynomial term
 typedef struct node {
     int coef;
     int xexp, yexp, zexp;
     struct node *next;
 } *NODE;
 
-/* ------------------------ GET NEW NODE ------------------------ */
+/* -------------------------------------- */
 NODE getnode() {
     NODE n = (NODE)malloc(sizeof(struct node));
-    if (n == NULL) {
+    if (!n) {
         printf("Memory allocation failed!\n");
         exit(0);
     }
     return n;
 }
 
-/* ------------------------ ATTACH TERM ------------------------- */
+/* -------------------------------------- */
 NODE attach(int c, int x, int y, int z, NODE head) {
     NODE temp = getnode();
     temp->coef = c;
@@ -29,9 +28,8 @@ NODE attach(int c, int x, int y, int z, NODE head) {
     temp->yexp = y;
     temp->zexp = z;
 
-    NODE cur = head;
-    while (cur->next != head)
-        cur = cur->next;
+    NODE cur;
+    for (cur = head; cur->next != head; cur = cur->next);
 
     cur->next = temp;
     temp->next = head;
@@ -39,9 +37,9 @@ NODE attach(int c, int x, int y, int z, NODE head) {
     return head;
 }
 
-/* ------------------------ READ POLYNOMIAL ------------------------- */
+/* -------------------------------------- */
 NODE read_poly(NODE head) {
-    int n, coef, x, y, z;
+    int n, c, x, y, z;
 
     printf("\nEnter number of terms: ");
     scanf("%d", &n);
@@ -50,59 +48,59 @@ NODE read_poly(NODE head) {
         printf("\nTerm %d:", i);
 
         printf("\n  Coef: ");
-        scanf("%d", &coef);
+        scanf("%d", &c);
 
         printf("  Powers of x y z: ");
         scanf("%d %d %d", &x, &y, &z);
 
-        head = attach(coef, x, y, z, head);
+        head = attach(c, x, y, z, head);
     }
+
     return head;
 }
 
-/* ------------------------ DISPLAY POLYNOMIAL ---------------------- */
+/* -------------------------------------- */
 void display(NODE head) {
     if (head->next == head) {
-        printf("\nPolynomial is empty.\n");
+        printf("\nPolynomial is empty.");
         return;
     }
 
-    NODE cur = head->next;
-    while (cur != head) {
-        printf("%d x^%d y^%d z^%d", cur->coef, cur->xexp, cur->yexp, cur->zexp);
-        cur = cur->next;
+    NODE cur;
+    for (cur = head->next; cur != head; cur = cur->next) {
+        printf("%d x^%d y^%d z^%d",
+               cur->coef, cur->xexp, cur->yexp, cur->zexp);
 
-        if (cur != head) printf(" + ");
+        if (cur->next != head) printf(" + ");
     }
 }
 
-/* ------------------------ EVALUATE POLYNOMIAL ---------------------- */
+/* -------------------------------------- */
 int poly_evaluate(NODE head) {
-    int x, y, z, sum = 0;
+    int x, y, z, result = 0;
 
-    printf("\nEnter values of x, y, z: ");
+    printf("\nEnter x y z values: ");
     scanf("%d %d %d", &x, &y, &z);
 
-    NODE cur = head->next;
-
-    while (cur != head) {
-        sum += cur->coef *
-               pow(x, cur->xexp) *
-               pow(y, cur->yexp) *
-               pow(z, cur->zexp);
-
-        cur = cur->next;
+    NODE cur;
+    for (cur = head->next; cur != head; cur = cur->next) {
+        result += cur->coef *
+                  pow(x, cur->xexp) *
+                  pow(y, cur->yexp) *
+                  pow(z, cur->zexp);
     }
 
-    return sum;
+    return result;
 }
 
-/* ------------------------ ADD TWO POLYNOMIALS ---------------------- */
+/* -------------------------------------- */
 NODE poly_sum(NODE h1, NODE h2, NODE h3) {
     NODE a = h1->next;
     NODE b = h2->next;
 
-    while (a != h1 && b != h2) {
+    while (1) {
+        if (a == h1 || b == h2) break;
+
         if (a->xexp == b->xexp &&
             a->yexp == b->yexp &&
             a->zexp == b->zexp)
@@ -114,8 +112,8 @@ NODE poly_sum(NODE h1, NODE h2, NODE h3) {
             b = b->next;
         }
         else if (a->xexp > b->xexp ||
-                 (a->xexp == b->xexp && a->yexp > b->yexp) ||
-                 (a->xexp == b->xexp && a->yexp == b->yexp && a->zexp > b->zexp))
+                (a->xexp == b->xexp && a->yexp > b->yexp) ||
+                (a->xexp == b->xexp && a->yexp == b->yexp && a->zexp > b->zexp))
         {
             h3 = attach(a->coef, a->xexp, a->yexp, a->zexp, h3);
             a = a->next;
@@ -126,63 +124,50 @@ NODE poly_sum(NODE h1, NODE h2, NODE h3) {
         }
     }
 
-    while (a != h1) {
+    for (; a != h1; a = a->next)
         h3 = attach(a->coef, a->xexp, a->yexp, a->zexp, h3);
-        a = a->next;
-    }
 
-    while (b != h2) {
+    for (; b != h2; b = b->next)
         h3 = attach(b->coef, b->xexp, b->yexp, b->zexp, h3);
-        b = b->next;
-    }
 
     return h3;
 }
 
-/* --------------------------- MAIN --------------------------- */
+/* -------------------------------------- */
 int main() {
-    NODE head, head1, head2, head3;
-    int choice;
+    NODE head  = getnode(); head->next  = head;
+    NODE head1 = getnode(); head1->next = head1;
+    NODE head2 = getnode(); head2->next = head2;
+    NODE head3 = getnode(); head3->next = head3;
 
-    head = getnode();   head->next = head;
-    head1 = getnode();  head1->next = head1;
-    head2 = getnode();  head2->next = head2;
-    head3 = getnode();  head3->next = head3;
+    int ch;
 
     while (1) {
-        printf("\n--------- MENU ---------");
-        printf("\n1. Represent & Evaluate P(x,y,z)");
+        printf("\n------ MENU ------");
+        printf("\n1. Evaluate Polynomial");
         printf("\n2. Add Two Polynomials");
         printf("\n3. Exit");
         printf("\nEnter choice: ");
-        scanf("%d", &choice);
+        scanf("%d", &ch);
 
-        switch (choice) {
+        switch (ch) {
             case 1:
                 head = read_poly(head);
-
-                printf("\nPolynomial:\n");
+                printf("\nPolynomial: ");
                 display(head);
-
                 printf("\nResult = %d\n", poly_evaluate(head));
                 break;
 
             case 2:
-                printf("\nEnter Polynomial 1:");
+                printf("\nEnter POLY1:");
                 head1 = read_poly(head1);
 
-                printf("\nPolynomial 1:\n");
-                display(head1);
-
-                printf("\n\nEnter Polynomial 2:");
+                printf("\nEnter POLY2:");
                 head2 = read_poly(head2);
-
-                printf("\nPolynomial 2:\n");
-                display(head2);
 
                 head3 = poly_sum(head1, head2, head3);
 
-                printf("\n\nResultant Polynomial:\n");
+                printf("\nSum = ");
                 display(head3);
                 break;
 
@@ -190,6 +175,4 @@ int main() {
                 exit(0);
         }
     }
-
-    return 0;
 }
